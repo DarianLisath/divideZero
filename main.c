0475996e44cbd656b81b0c6a727b376a6b6f52cc
Darian Lisath <dl023416@ohio.edu>
2018-12-16 23:12:17 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int num1,num2,cutter;
+
+	printf("Enter two numbers:");
+
+	scanf("%d %d",&num1,&num2);
+cutter=num1/num2;
+    if("%d == 0")
+
+    printf("Cannot divide by Zero!");
+
+    else
+
+
+    printf("Answer: %d",cutter);
+
+
+	return  0;
+}
